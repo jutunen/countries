3cdc3a5c6fd5df21c6ddf0b0b6e028f970d9456e
Jussi Utunen <jussi.utunen@gmail.com>
2020-12-17 18:59:52 +0200

    add region selector

--- src/App.js
+++ src/App.js
@@ -7,29 +7,60 @@ import countries from './all_countries.json';
 
 function App() {
 
+  const ALL_REGIONS_SELECTED = "All";
+  const UNCATEGORIZED_REGION = "Uncategorized";
   const [country,setCountry] = useState("");
+  const [region,setRegion] = useState(ALL_REGIONS_SELECTED);
+
+  let regionsSet = new Set(countries.map(x => x.subregion));
+  let regions = Array.from(regionsSet);
+  regions.push(ALL_REGIONS_SELECTED);
+  let indexOfEmptyValue = regions.indexOf("");
+  regions[indexOfEmptyValue] = UNCATEGORIZED_REGION;
+  regions.sort();
 
   function handleCountry(event) {
     setCountry(event.target.value);
   }
 
-  function filterFunction(country, searchStr) {
+  function changeRegion(event) {
+    setRegion(event.target.value);
+  }
 
+  function filterFunction(countryName, countryRegion, searchStr, selectedRegion) {
+
+    /*
     if(!searchStr) {
       return false;
+    }
+    */
+
+    if(selectedRegion === UNCATEGORIZED_REGION) {
+      selectedRegion = "";
+    }
+
+    let regionMatches;
+
+    if(selectedRegion === ALL_REGIONS_SELECTED) {
+      regionMatches = true;
+    } else if( countryRegion === selectedRegion) {
+      regionMatches = true;
+    } else {
+      regionMatches = false;
     }
 
     let regex;
 
-    if(searchStr.length === 1) {
+    if(searchStr.length < 3) {
       regex = new RegExp("^" + searchStr, "gi");
     } else {
       regex = new RegExp(searchStr, "gi");
     }
 
-    let rval = country.match(regex);
+    let rval = countryName.match(regex);
+    //let rval_2 = nativeName.match(regex);
 
-    if(rval === null) {
+    if(rval === null || !regionMatches) {
       return false;
     }
 
@@ -59,7 +90,7 @@ function App() {
     return (
       <div id="table">
         {/*{props.countries.slice(0, props.countries.length).map( x =>*/}
-        {props.countries.filter( x => filterFunction(x.name, props.searchStr) ).map( x =>
+        {props.countries.filter( x => filterFunction(x.name, x.subregion, props.searchStr, props.region) ).map( x =>
           {
             return (
               <TableRow
@@ -84,11 +115,18 @@ function App() {
           value={country}
           onChange={ev => handleCountry(ev)}
         />
+        <select
+          value={region}
+          onChange={ev => changeRegion(ev)}
+        >
+          {regions.map( region => <option key={region} value={region}>{region}</option> )}
+        </select>
       </div>
       <div>
         <Table
           countries={countries}
           searchStr={country}
+          region={region}
         >
         </Table>
       </div>
