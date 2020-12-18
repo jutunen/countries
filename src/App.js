7efb5d9b8f21c4a1f70f4c2a604ffa688eb1fb32
Jussi Utunen <jussi.utunen@gmail.com>
2020-12-18 04:11:43 +0200

    split App.js to Table.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import {useState, useEffect, useRef} from "react";
 import './App.css';
 import axios from 'axios';
 import COUNTRIES from './all_countries.json';
-import * as cloneDeep from 'lodash.clonedeep';
+import {Table} from "./Table.js";
 
 // https://restcountries.eu/rest/v2/all
 
+export const ALL_REGIONS_SELECTED = "All";
+export const UNCATEGORIZED_REGION = "Uncategorized";
+
 function App() {
 
-  const ALL_REGIONS_SELECTED = "All";
-  const UNCATEGORIZED_REGION = "Uncategorized";
   const [searchStr,setSearchStr] = useState("");
   const [selectedRegion,setSelectedRegion] = useState(ALL_REGIONS_SELECTED);
   const [allCountries,setAllCountries] = useState(COUNTRIES);
@@ -58,10 +59,6 @@ function App() {
     }
   }
 
-  function handleCountryDetailsClick(code) {
-    window.location.hash = code;
-  }
-
   function CountryDetails(props) {
 
     if(props.code === undefined) {
@@ -86,142 +83,6 @@ function App() {
             <button className="button" type="button" onClick={ props.closeCb }> Close </button>
           </div>
         </div>
-    )
-  }
-
-  function filterFunction(countryName, countryRegion, searchStr, selectedRegion) {
-
-    /*
-    if(!searchStr) {
-      return false;
-    }
-    */
-
-    if(selectedRegion === UNCATEGORIZED_REGION) {
-      selectedRegion = "";
-    }
-
-    let regionMatches;
-
-    if(selectedRegion === ALL_REGIONS_SELECTED) {
-      regionMatches = true;
-    } else if( countryRegion === selectedRegion) {
-      regionMatches = true;
-    } else {
-      regionMatches = false;
-    }
-
-    let regex;
-
-    if(searchStr.length < 3) {
-      regex = new RegExp("^" + searchStr, "gi");
-    } else {
-      regex = new RegExp(searchStr, "gi");
-    }
-
-    let rval = countryName.match(regex);
-    //let rval_2 = nativeName.match(regex);
-
-    if(rval === null || !regionMatches) {
-      return false;
-    }
-
-    return true;
-  }
-
-  function TableRow(props) {
-    return (
-      <div className="countryline" onClick={() => handleCountryDetailsClick(props.code)}>
-        <div className="countrydetail flag">
-          {/*<img src={props.flag} alt={""} height={50} width={100} />*/}
-          <img src='https://restcountries.eu/data/afg.svg' alt={""} height={50} width={100} />
-        </div>
-        <div className="countrydetail name">
-          {props.name}
-        </div>
-        <div className="countrydetail subregion">
-          {props.subregion}
-        </div>
-        <div className="countrydetail population">
-          {props.population}
-        </div>
-      </div>
-    )
-  }
-
-  function TableHeader() {
-
-    function sortByCountryName() {
-      let allCountriesClone = cloneDeep(allCountries);
-      allCountriesClone.sort((a,b) => {
-        if(a.name < b.name) { return (sortBool ? 1 : -1) }
-        if(a.name > b.name) { return (sortBool ? -1 : 1) }
-        return 0;
-      });
-      setAllCountries(allCountriesClone);
-      setSortBool(!sortBool);
-    }
-
-    function sortByRegion() {
-      let allCountriesClone = cloneDeep(allCountries);
-      allCountriesClone.sort((a,b) => {
-        if(a.subregion < b.subregion) { return (sortBool ? 1 : -1) }
-        if(a.subregion > b.subregion) { return (sortBool ? -1 : 1) }
-        return 0;
-      });
-      setAllCountries(allCountriesClone);
-      setSortBool(!sortBool);
-    }
-
-    function sortByPopulation() {
-      let allCountriesClone = cloneDeep(allCountries);
-      allCountriesClone.sort((a,b) => { return (sortBool ? a.population - b.population : b.population - a.population) } );
-      setAllCountries(allCountriesClone);
-      setSortBool(!sortBool);
-    }
-
-    return (
-      <div id="tableHeader" className="countryline">
-        <div className="tableHeaderCell flag">
-
-        </div>
-        <div className="tableHeaderCell name" onClick={sortByCountryName}>
-          Country
-        </div>
-        <div className="tableHeaderCell subregion" onClick={sortByRegion}>
-          Region
-        </div>
-        <div className="tableHeaderCell population" onClick={sortByPopulation}>
-          Population
-        </div>
-      </div>
-    )
-  }
-
-  function Table(props) {
-
-    return (
-      <div id="tableContainer">
-        <TableHeader />
-        <div id="tableBody">
-          {/*{props.allCountries.slice(0, props.allCountries.length).map( x =>*/}
-          {props.allCountries.filter( x => filterFunction(x.name, x.subregion, props.searchStr, props.selectedRegion) ).map( x =>
-            {
-              return (
-                <TableRow
-                  key={x.name}
-                  flag={x.flag}
-                  name={x.name}
-                  subregion={x.subregion === "" ? UNCATEGORIZED_REGION : x.subregion}
-                  population={x.population}
-                  code={x.numericCode}
-                >
-                </TableRow>
-              )
-            })
-          }
-        </div>
-      </div>
     )
   }
 
@@ -261,16 +122,18 @@ function App() {
       <div>
         <Table
           allCountries={allCountries}
+          setAllCountries={setAllCountries}
+          sortBool={sortBool}
+          setSortBool={setSortBool}
           searchStr={searchStr}
           selectedRegion={selectedRegion}
-        >
-        </Table>
+        />
+
       </div>
       <CountryDetails
         code={countryCode}
         closeCb={() => window.location.hash = "main"}
-      >
-      </CountryDetails>
+      />
     </div>
   );
 }
---
+++ src/Table.js
@@ -0,0 +1,141 @@
+import './App.css';
+import * as cloneDeep from 'lodash.clonedeep';
+import {ALL_REGIONS_SELECTED, UNCATEGORIZED_REGION} from "./App.js";
+
+function filterFunction(countryName, countryRegion, searchStr, selectedRegion) {
+
+  /*
+  if(!searchStr) {
+    return false;
+  }
+  */
+
+  if(selectedRegion === UNCATEGORIZED_REGION) {
+    selectedRegion = "";
+  }
+
+  let regionMatches;
+
+  if(selectedRegion === ALL_REGIONS_SELECTED) {
+    regionMatches = true;
+  } else if( countryRegion === selectedRegion) {
+    regionMatches = true;
+  } else {
+    regionMatches = false;
+  }
+
+  let regex;
+
+  if(searchStr.length < 3) {
+    regex = new RegExp("^" + searchStr, "gi");
+  } else {
+    regex = new RegExp(searchStr, "gi");
+  }
+
+  let rval = countryName.match(regex);
+
+  if(rval === null || !regionMatches) {
+    return false;
+  }
+
+  return true;
+}
+
+function TableRow(props) {
+  return (
+    <div className="countryline" onClick={() => window.location.hash = props.code}>
+      <div className="countrydetail flag">
+        {/*<img src={props.flag} alt={""} height={50} width={100} />*/}
+        <img src='https://restcountries.eu/data/afg.svg' alt={""} height={50} width={100} />
+      </div>
+      <div className="countrydetail name">
+        {props.name}
+      </div>
+      <div className="countrydetail subregion">
+        {props.subregion}
+      </div>
+      <div className="countrydetail population">
+        {props.population}
+      </div>
+    </div>
+  )
+}
+
+function TableHeader(props) {
+
+  function sortByCountryName() {
+    let allCountriesClone = cloneDeep(props.allCountries);
+    allCountriesClone.sort((a,b) => {
+      if(a.name < b.name) { return (props.sortBool ? 1 : -1) }
+      if(a.name > b.name) { return (props.sortBool ? -1 : 1) }
+      return 0;
+    });
+    props.setAllCountries(allCountriesClone);
+    props.setSortBool(!props.sortBool);
+  }
+
+  function sortByRegion() {
+    let allCountriesClone = cloneDeep(props.allCountries);
+    allCountriesClone.sort((a,b) => {
+      if(a.subregion < b.subregion) { return (props.sortBool ? 1 : -1) }
+      if(a.subregion > b.subregion) { return (props.sortBool ? -1 : 1) }
+      return 0;
+    });
+    props.setAllCountries(allCountriesClone);
+    props.setSortBool(!props.sortBool);
+  }
+
+  function sortByPopulation() {
+    let allCountriesClone = cloneDeep(props.allCountries);
+    allCountriesClone.sort((a,b) => { return (props.sortBool ? a.population - b.population : b.population - a.population) } );
+    props.setAllCountries(allCountriesClone);
+    props.setSortBool(!props.sortBool);
+  }
+
+  return (
+    <div id="tableHeader" className="countryline">
+      <div className="tableHeaderCell flag">
+
+      </div>
+      <div className="tableHeaderCell name" onClick={sortByCountryName}>
+        Country
+      </div>
+      <div className="tableHeaderCell subregion" onClick={sortByRegion}>
+        Region
+      </div>
+      <div className="tableHeaderCell population" onClick={sortByPopulation}>
+        Population
+      </div>
+    </div>
+  )
+}
+
+export function Table(props) {
+
+  return (
+    <div id="tableContainer">
+      <TableHeader
+        allCountries={props.allCountries}
+        setAllCountries={props.setAllCountries}
+        sortBool={props.sortBool}
+        setSortBool={props.setSortBool}
+      />
+      <div id="tableBody">
+        {props.allCountries.filter( x => filterFunction(x.name, x.subregion, props.searchStr, props.selectedRegion) ).map( x =>
+          {
+            return (
+              <TableRow
+                key={x.name}
+                flag={x.flag}
+                name={x.name}
+                subregion={x.subregion === "" ? UNCATEGORIZED_REGION : x.subregion}
+                population={x.population}
+                code={x.numericCode}
+              />
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
